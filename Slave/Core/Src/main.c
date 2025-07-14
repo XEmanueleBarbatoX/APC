d1013dec17aba73566fd9de52f32339483a70b88
Matteo Arnese <68701124+xMattMaster@users.noreply.github.com>
2025-07-14 19:23:28 +0200

    Project refactoring and mutual exlusion error correction
    Modified the repo structure so that the two STM32CubeIDE projects,
    Master and Slave, are both on the root directory.
    Modified Master's main.c to implement mutual exclusion differently,
    using __STREXW and __LDREXW.

--- Slave/Core/Src/main.c
+++ Slave/Core/Src/main.c
@@ -21,12 +21,10 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <string.h>
 #include <stdio.h>
-#include <inttypes.h>
 #include "ssd1306/ssd1306.h"
 #include "ssd1306/ssd1306_fonts.h"
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -36,6 +34,10 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define REG_TEMP		   0x00
+#define REG_CONF           0x01
+#define REG_THYST          0x02
+#define REG_TOS            0x03
 
 /* USER CODE END PD */
 
@@ -45,15 +47,31 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+
 I2C_HandleTypeDef hi2c1;
 
-TIM_HandleTypeDef htim3;
-
+UART_HandleTypeDef hlpuart1;
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
-UART_HandleTypeDef huart6;
 
 /* USER CODE BEGIN PV */
+static const uint8_t LM75A = 0x48 << 1;
+
+// Stati del sistema
+typedef enum {
+	STATE_NORMAL,
+	STATE_WAIT_RESPONSE,
+	STATE_RESPONSE_RECEIVED,
+	STATE_POST_RESPONSE_WAIT
+} SystemState_t;
+
+volatile SystemState_t system_state = STATE_NORMAL;
+volatile uint8_t rx_data_ready = 1;
+uint8_t rxBuf[8];
+uint32_t postResponseTick;
+
+static const char alarmMsg[] = "FRALRM\r\n";
+static uint32_t lastPress = 0;
 
 /* USER CODE END PV */
 
@@ -61,22 +79,44 @@ UART_HandleTypeDef huart6;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
-static void MX_USART1_UART_Init(void);
-static void MX_USART6_UART_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_TIM3_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_LPUART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
-
-#define DELAY_TICKS 30000U //delay ticks for TIM3
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-uint8_t rx1[8], rx6[8]; // Buffers for UART1 and UART6
-volatile uint32_t incidents = 0; // Incidents counter
+// Ridefinizione Callback di ricezione UART (interrupt)
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	// if (huart->Instance == USART1 && system_state == STATE_WAIT_RESPONSE) {
+	// if (huart->Instance == USART1) {
+	if (huart->Instance == USART1) {
+		// terminatore stringa
+		//rxBuf[8] = '\0';
+		system_state = STATE_RESPONSE_RECEIVED;
+		rx_data_ready = 1;
 
+		// Preparazione USART prossima ricezione
+		HAL_UART_Receive_IT(&huart1, rxBuf, 8);
+	}
+}
+
+// Ridefinizione Callback Interruzione Pressione Pulsante
+void BSP_PB_Callback(Button_TypeDef Button) {
+	if (Button == BUTTON_USER && system_state == STATE_RESPONSE_RECEIVED) {
+		uint32_t now = HAL_GetTick();
+		if (now - lastPress > 200) // debounce delay 200 ms
+				{
+			lastPress = now;
+			system_state = STATE_NORMAL;
+			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+
+		}
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -86,7 +126,8 @@ volatile uint32_t incidents = 0; // Incidents counter
 int main(void) {
 
 	/* USER CODE BEGIN 1 */
-
+	int cont = 0;
+	float soglia = 30.0f;
 	/* USER CODE END 1 */
 
 	/* MCU Configuration--------------------------------------------------------*/
@@ -108,43 +149,137 @@ int main(void) {
 	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
 	MX_I2C1_Init();
-	MX_USART1_UART_Init();
-	MX_USART6_UART_Init();
 	MX_USART2_UART_Init();
-	MX_TIM3_Init();
+	MX_USART1_UART_Init();
+	MX_LPUART1_UART_Init();
 	/* USER CODE BEGIN 2 */
+	HAL_NVIC_SetPriority(USART1_IRQn, 1, 0);
+	HAL_NVIC_EnableIRQ(USART1_IRQn);
 
-	HAL_TIM_Base_Start(&htim3);
-	HAL_UART_Receive_IT(&huart1, rx1, 8);
-	HAL_UART_Receive_IT(&huart6, rx6, 8);
-
+	// Preparazione USART
+	// __HAL_UART_ENABLE_IT(&huart1,UART_IT_RXNE);
+	HAL_UART_Receive_IT(&huart1, rxBuf, 8);
 	ssd1306_Init();
 
-	// Clear the OLED display
-	ssd1306_Fill(Black);
-	ssd1306_UpdateScreen();
-
-	char incidents_string[11] = "Undefined!";
-
 	/* USER CODE END 2 */
+
+	/* Initialize led */
+	BSP_LED_Init(LED_GREEN);
+
+	/* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
+	BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
 	while (1) {
+
+		if (rx_data_ready == 1) {
+
+			rx_data_ready = 0;
+
+			if (system_state == STATE_WAIT_RESPONSE) {
+				system_state = STATE_RESPONSE_RECEIVED;
+			}
+		}
+
+		switch (system_state) {
+
+		case STATE_NORMAL: {
+			// Modalità normale: leggo e stampo la temperatura
+			uint8_t cmd = REG_TEMP;
+			if (HAL_I2C_Master_Transmit(&hi2c1, LM75A, &cmd, 1, HAL_MAX_DELAY)
+					== HAL_OK) {
+				uint8_t data[2];
+				if (HAL_I2C_Master_Receive(&hi2c1, LM75A, data, 2,
+				HAL_MAX_DELAY) == HAL_OK) {
+					int16_t raw = ((int16_t) data[0] << 8) | data[1];
+					raw >>= 5;
+					float temp = raw * 0.125f;
+					char temp_s[16];
+					snprintf(temp_s, sizeof(temp_s), "T: %.2f C", temp);
+
+					ssd1306_Fill(Black);
+					ssd1306_SetCursor(2, 2);
+					ssd1306_WriteString("Temperatura corrente:", Font_6x8,
+							White);
+					ssd1306_SetCursor(2, 20);
+					ssd1306_WriteString(temp_s, Font_7x10, Black);
+					ssd1306_UpdateScreen();
+
+					if (temp > soglia) {
+						cont++;
+						if (cont == 4) {
+							cont = 0;
+							// Invio l'allarme e lo stampo
+							// const char alarmMsg[] = "ALLARME Fratm\r\n";
+
+							HAL_UART_Transmit(&huart1, (uint8_t*) alarmMsg, 8,
+							HAL_MAX_DELAY);
+
+							ssd1306_Fill(Black);
+							ssd1306_SetCursor(2, 20);
+							ssd1306_WriteString("ALLARME INCENDIO", Font_7x10,
+									White);
+							ssd1306_UpdateScreen();
+							HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+
+							// passo a stato di attesa risposta
+							system_state = STATE_WAIT_RESPONSE;
+						}
+					} else {
+						cont = 0;
+					}
+				}
+			}
+			HAL_Delay(100);
+			break;
+		}
+
+		case STATE_WAIT_RESPONSE: {
+
+			uint8_t cmd = REG_TEMP;
+			if (HAL_I2C_Master_Transmit(&hi2c1, LM75A, &cmd, 1, HAL_MAX_DELAY)
+					== HAL_OK) {
+				uint8_t data[2];
+				if (HAL_I2C_Master_Receive(&hi2c1, LM75A, data, 2,
+				HAL_MAX_DELAY) == HAL_OK) {
+					int16_t raw = ((int16_t) data[0] << 8) | data[1];
+					raw >>= 5;
+					float temp = raw * 0.125f;
+					char temp_s[16];
+					snprintf(temp_s, sizeof(temp_s), "T: %.2f C", temp);
+
+					ssd1306_SetCursor(40, 45);
+					ssd1306_WriteString(temp_s, Font_7x10, Black);
+					ssd1306_UpdateScreen();
+
+				}
+			}
+			HAL_Delay(100);
+			break;
+		}
+
+		case STATE_RESPONSE_RECEIVED: {
+			// Appena arriva la risposta UART (gestita in RxCpltCallback)
+			ssd1306_Fill(Black);
+			ssd1306_SetCursor(20, 18);
+			ssd1306_WriteString("SOCCORSI", Font_11x18, White);
+			ssd1306_SetCursor(14, 37);
+			ssd1306_WriteString("IN ARRIVO", Font_11x18, White);
+			ssd1306_UpdateScreen();
+
+			break;
+		}
+
+		default: {
+			break;
+		}
+
+		}
+
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
-
-		// Display the number of incidents on the OLED
-		ssd1306_SetCursor(2, 0);
-		ssd1306_WriteString("Numero incidenti:", Font_7x10, White);
-		ssd1306_SetCursor(2, 18);
-		snprintf(incidents_string, sizeof(incidents_string), "%" PRIu32,
-				incidents);
-		ssd1306_WriteString(incidents_string, Font_11x18, White);
-		ssd1306_UpdateScreen();
-
-		HAL_Delay(30);
 	}
 	/* USER CODE END 3 */
 }
@@ -159,8 +294,7 @@ void SystemClock_Config(void) {
 
 	/** Configure the main internal regulator output voltage
 	 */
-	__HAL_RCC_PWR_CLK_ENABLE();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+	HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
 
 	/** Initializes the RCC Oscillators according to the specified parameters
 	 * in the RCC_OscInitTypeDef structure.
@@ -170,10 +304,11 @@ void SystemClock_Config(void) {
 	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
 	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
 	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	RCC_OscInitStruct.PLL.PLLM = 16;
-	RCC_OscInitStruct.PLL.PLLN = 336;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-	RCC_OscInitStruct.PLL.PLLQ = 7;
+	RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
+	RCC_OscInitStruct.PLL.PLLN = 85;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
 	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
 		Error_Handler();
 	}
@@ -184,10 +319,10 @@ void SystemClock_Config(void) {
 			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
 	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
 	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
 		Error_Handler();
 	}
 }
@@ -207,15 +342,28 @@ static void MX_I2C1_Init(void) {
 
 	/* USER CODE END I2C1_Init 1 */
 	hi2c1.Instance = I2C1;
-	hi2c1.Init.ClockSpeed = 100000;
-	hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+	hi2c1.Init.Timing = 0x40B285C2;
 	hi2c1.Init.OwnAddress1 = 0;
 	hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
 	hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
 	hi2c1.Init.OwnAddress2 = 0;
+	hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
 	hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
 	hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
 	if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Configure Analogue filter
+	 */
+	if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Configure Digital filter
+	 */
+	if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK) {
 		Error_Handler();
 	}
 	/* USER CODE BEGIN I2C1_Init 2 */
@@ -225,59 +373,46 @@ static void MX_I2C1_Init(void) {
 }
 
 /**
- * @brief TIM3 Initialization Function
+ * @brief LPUART1 Initialization Function
  * @param None
  * @retval None
  */
-static void MX_TIM3_Init(void) {
+static void MX_LPUART1_UART_Init(void) {
 
-	/* USER CODE BEGIN TIM3_Init 0 */
+	/* USER CODE BEGIN LPUART1_Init 0 */
 
-	/* USER CODE END TIM3_Init 0 */
+	/* USER CODE END LPUART1_Init 0 */
 
-	TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
-	TIM_MasterConfigTypeDef sMasterConfig = { 0 };
-	TIM_OC_InitTypeDef sConfigOC = { 0 };
+	/* USER CODE BEGIN LPUART1_Init 1 */
 
-	/* USER CODE BEGIN TIM3_Init 1 */
-
-	/* USER CODE END TIM3_Init 1 */
-	htim3.Instance = TIM3;
-	htim3.Init.Prescaler = 8399;
-	htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-	htim3.Init.Period = 65535;
-	htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-	htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-	if (HAL_TIM_Base_Init(&htim3) != HAL_OK) {
+	/* USER CODE END LPUART1_Init 1 */
+	hlpuart1.Instance = LPUART1;
+	hlpuart1.Init.BaudRate = 209700;
+	hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
+	hlpuart1.Init.StopBits = UART_STOPBITS_1;
+	hlpuart1.Init.Parity = UART_PARITY_NONE;
+	hlpuart1.Init.Mode = UART_MODE_TX_RX;
+	hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	hlpuart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+	hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	if (HAL_UART_Init(&hlpuart1) != HAL_OK) {
 		Error_Handler();
 	}
-	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-	if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK) {
-		Error_Handler();
-	}
-	if (HAL_TIM_OC_Init(&htim3) != HAL_OK) {
-		Error_Handler();
-	}
-	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-	if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig)
+	if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8)
 			!= HAL_OK) {
 		Error_Handler();
 	}
-	sConfigOC.OCMode = TIM_OCMODE_TIMING;
-	sConfigOC.Pulse = 0;
-	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-	if (HAL_TIM_OC_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK) {
+	if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8)
+			!= HAL_OK) {
 		Error_Handler();
 	}
-	if (HAL_TIM_OC_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK) {
+	if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK) {
 		Error_Handler();
 	}
-	/* USER CODE BEGIN TIM3_Init 2 */
+	/* USER CODE BEGIN LPUART1_Init 2 */
 
-	/* USER CODE END TIM3_Init 2 */
-	HAL_TIM_MspPostInit(&htim3);
+	/* USER CODE END LPUART1_Init 2 */
 
 }
 
@@ -303,7 +438,21 @@ static void MX_USART1_UART_Init(void) {
 	huart1.Init.Mode = UART_MODE_TX_RX;
 	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
 	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+	huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+	huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
 	if (HAL_UART_Init(&huart1) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK) {
 		Error_Handler();
 	}
 	/* USER CODE BEGIN USART1_Init 2 */
@@ -334,43 +483,26 @@ static void MX_USART2_UART_Init(void) {
 	huart2.Init.Mode = UART_MODE_TX_RX;
 	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
 	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+	huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
 	if (HAL_UART_Init(&huart2) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_1_8)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_1_8)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_DisableFifoMode(&huart2) != HAL_OK) {
 		Error_Handler();
 	}
 	/* USER CODE BEGIN USART2_Init 2 */
 
 	/* USER CODE END USART2_Init 2 */
-
-}
-
-/**
- * @brief USART6 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_USART6_UART_Init(void) {
-
-	/* USER CODE BEGIN USART6_Init 0 */
-
-	/* USER CODE END USART6_Init 0 */
-
-	/* USER CODE BEGIN USART6_Init 1 */
-
-	/* USER CODE END USART6_Init 1 */
-	huart6.Instance = USART6;
-	huart6.Init.BaudRate = 115200;
-	huart6.Init.WordLength = UART_WORDLENGTH_8B;
-	huart6.Init.StopBits = UART_STOPBITS_1;
-	huart6.Init.Parity = UART_PARITY_NONE;
-	huart6.Init.Mode = UART_MODE_TX_RX;
-	huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-	huart6.Init.OverSampling = UART_OVERSAMPLING_16;
-	if (HAL_UART_Init(&huart6) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN USART6_Init 2 */
-
-	/* USER CODE END USART6_Init 2 */
 
 }
 
@@ -387,25 +519,19 @@ static void MX_GPIO_Init(void) {
 
 	/* GPIO Ports Clock Enable */
 	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOF_CLK_ENABLE();
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
 
-	/*Configure GPIO pin : B1_Pin */
-	GPIO_InitStruct.Pin = B1_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-	/*Configure GPIO pin : LD2_Pin */
-	GPIO_InitStruct.Pin = LD2_Pin;
+	/*Configure GPIO pin : PB5 */
+	GPIO_InitStruct.Pin = GPIO_PIN_5;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 	/* USER CODE BEGIN MX_GPIO_Init_2 */
 
@@ -413,62 +539,6 @@ static void MX_GPIO_Init(void) {
 }
 
 /* USER CODE BEGIN 4 */
-
-/**
- * @brief  Rx Transfer completed callbacks.
- * @param  huart  Pointer to a UART_HandleTypeDef structure that contains
- *                the configuration information for the specified UART module.
- * @retval None
- */
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-	if (huart->Instance == USART1 || huart->Instance == USART6) {
-
-		// Increment the incidents counter atomically
-		uint32_t buffer, result;
-		do {
-			buffer = __LDREXW(&incidents);
-			buffer += 1;
-			result = __STREXW(buffer, &incidents);
-		} while (result != 0);
-
-		// Set timer for 3 seconds
-		uint32_t now = __HAL_TIM_GET_COUNTER(&htim3);
-		uint32_t next = (now + DELAY_TICKS) % (htim3.Init.Period + 1);
-
-		if (huart->Instance == USART1) {
-			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, next);
-			HAL_TIM_OC_Start_IT(&htim3, TIM_CHANNEL_1);
-		} else if (huart->Instance == USART6) {
-			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, next);
-			HAL_TIM_OC_Start_IT(&htim3, TIM_CHANNEL_2);
-		}
-
-		// Restart UART reception
-		HAL_UART_Receive_IT(huart, (huart->Instance == USART1) ? rx1 : rx6, 8);
-	}
-}
-
-/**
- * @brief  Output Compare callback in non-blocking mode
- * @param  htim TIM OC handle
- * @retval None
- */
-void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim) {
-	if (htim->Instance != TIM3)
-		return;
-
-	// Send a message on the appropriate UART when the timer expires
-	uint8_t msg[] = "HLPSNT\r\n";
-	if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1) {
-		// 3 s elapsed for TIM3 CH1, send on USART1
-		HAL_UART_Transmit(&huart1, msg, sizeof(msg) - 1, HAL_MAX_DELAY);
-		HAL_TIM_OC_Stop_IT(&htim3, TIM_CHANNEL_1);
-	} else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2) {
-		// 3 s elapsed for TIM3 CH2, send on USART6
-		HAL_UART_Transmit(&huart6, msg, sizeof(msg) - 1, HAL_MAX_DELAY);
-		HAL_TIM_OC_Stop_IT(&htim3, TIM_CHANNEL_2);
-	}
-}
 
 /* USER CODE END 4 */
 
